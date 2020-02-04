dfab3a4b34b176041efdea844582f60e129a4bcc
adrianmontiveros <43603325+adrianmontiveros@users.noreply.github.com>
2020-02-04 10:30:18 +0100

    bubbles changes, added style.css and index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", start)
+
+function start() {
+    const bubbles = document.querySelectorAll("wrapper>div");
+
+    console.log(bubbles);
+    bubbles.forEach(bubble => {
+        const x = Math.random() * 100;
+        const y = Math.random() * 100;
+        bubble.style.transform = "translate(" + x + "vw," + x + "vh)";
+    })
+
+}
